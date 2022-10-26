54248308d54705cc21212b7c05122fafb31bde3c
Maciek <46852030+recostudio@users.noreply.github.com>
2022-10-26 22:16:36 +0200

    Main screen done programatically.

---
+++ CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class CWButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(bacgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = bacgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
